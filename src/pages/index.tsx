548156c364ca645d2a9a0fc7d5d66f20bfbda3de
Quentin Guenther <qguenther@outlook.com>
2021-06-08 21:40:52 -0700

    prototype hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,24 @@
-import Link from 'next/link';
-import Layout from '../components/Layout';
+import { GoGrabber } from 'react-icons/go';
 
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>
-      Hello Next.js{' '}
-      <span role="img" aria-label="hello wave">
-        👋
-      </span>
-    </h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
+  <div className="backdrop-filter backdrop-blur-sm bg-hero-pattern bg-cover bg-no-repeat bg-left-top h-96">
+    <div className="w-full h-full gradient">
+      <div className="grid grid-rows-3 grid-flow-row ">
+        <div className="flex justify-between px-6 py-10">
+          <h1 className="text-highlight font-bold leading-8 text-5xl">QG</h1>
+          <button className="neomorphic w-12 h-12 rounded-full flex justify-center items-center">
+            <GoGrabber className="text-white w-8 h-8" />
+          </button>
+        </div>
+        <h1 className="block self-end px-6 text-4xl text-accent-text">
+          I build amazing experiences.
+        </h1>
+        <p className="block self-start px-6 font-bold text-sm text-accent-text-muted">
+          Software Engineer from Seattle, WA
+        </p>
+      </div>
+    </div>
+  </div>
 );
 
 export default IndexPage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
+const withOpacity = (variableName) => {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+};
+
 module.exports = {
   purge: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -5,7 +14,20 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: () => ({
+        'hero-pattern': "url('../../public/images/hero.jpg')",
+      }),
+      colors: {
+        highlight: withOpacity('--color-highlight'),
+        'background-primary': withOpacity('--color-background-primary'),
+        'accent-text': withOpacity('--color-accent-text'),
+        'accent-text-muted': withOpacity('--color-accent-text-muted'),
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
