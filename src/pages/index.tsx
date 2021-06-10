f99448215e42a77ff56bde84d7b816ce462cee72
Quentin Guenther <qguenther@outlook.com>
2021-06-10 09:39:09 -0700

    prototype layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,26 +2,26 @@ import { GoGrabber, GoFileCode, GoLink } from 'react-icons/go';
 import { BsChatDots } from 'react-icons/bs';
 
 const IndexPage = () => (
-  <>
-    <div className="backdrop-filter backdrop-blur-sm bg-hero-pattern bg-cover bg-no-repeat bg-left-top h-96">
-      <div className="w-full h-full gradient">
-        <div className="grid grid-rows-3 grid-flow-row ">
-          <div className="flex justify-between px-6 py-10">
-            <h1 className="text-highlight font-bold leading-8 text-5xl">QG</h1>
-            <button className="neomorphic w-12 h-12 rounded-full flex justify-center items-center">
-              <GoGrabber className="text-white w-8 h-8" />
-            </button>
-          </div>
-          <h1 className="block self-end px-6 text-4xl text-accent-text">
+  <div className="flex flex-col h-screen">
+    <div className="flex-grow backdrop-filter backdrop-blur-sm bg-hero-pattern bg-cover bg-no-repeat bg-left-top h-72">
+      <div className="w-full h-full gradient relative">
+        <div className="flex justify-between px-6 pt-10 w-full">
+          <h1 className="text-highlight font-bold leading-8 text-5xl">QG</h1>
+          <button className="neomorphic w-12 h-12 rounded-full flex justify-center items-center">
+            <GoGrabber className="text-white w-8 h-8" />
+          </button>
+        </div>
+        <div className="absolute bottom-6">
+          <h1 className="block px-6 text-4xl text-accent-text">
             I build amazing experiences.
           </h1>
-          <p className="block self-start px-6 font-bold text-sm text-accent-text-muted">
+          <p className="block px-6 font-bold text-sm text-accent-text-muted">
             Software Engineer from Seattle, WA
           </p>
         </div>
       </div>
     </div>
-    <div className="bg-background-primary py-6 space-y-6">
+    <div className="flex-none bg-background-primary py-6 space-y-6">
       <div className="flex flex-row justify-between items-center px-6">
         <h1 className="block text-3xl text-accent-text">Recent Posts</h1>
         <a className=" block text-link text-sm font-bold bullet">Go to Blog</a>
@@ -75,7 +75,7 @@ const IndexPage = () => (
         <div className="h-4 w-4 flex-none rounded-full bg-muted shadow-md" />
       </div>
     </div>
-    <div className="bg-background-alt py-4 flex justify-around">
+    <div className="flex-none neomorphic bg-background-alt py-4 flex justify-around">
       <div className="flex flex-col items-center">
         <GoFileCode className="h-6 text-text" />
         <p className="text-text font-bold text-sm">BLOG</p>
@@ -89,7 +89,7 @@ const IndexPage = () => (
         <p className="text-text font-bold text-sm">CONTACT</p>
       </div>
     </div>
-  </>
+  </div>
 );
 
 export default IndexPage;
